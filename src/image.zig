3ee2e5ca1404577da9f9c78af044f15129b0eed0
andrewkraevskii <andrew.kraevskii@gmail.com>
2024-02-14 06:19:43 +0300

    ref: Image

---
+++ src/image.zig
@@ -0,0 +1,59 @@
+const std = @import("std");
+
+pub const Color = struct {
+    r: u8 = 0,
+    g: u8 = 0,
+    b: u8 = 0,
+    a: u8 = 255,
+    pub const WHITE = @This(){ .r = 255, .g = 255, .b = 255, .a = 255 };
+    pub const BLACK = @This(){ .r = 0, .g = 0, .b = 0, .a = 255 };
+
+    pub fn as_gray(self: @This()) u8 {
+        return @intCast((@as(u16, self.r) + @as(u16, self.g) + @as(u16, self.b)) / 3);
+    }
+};
+
+pub const Image = struct {
+    height: u16,
+    width: u16,
+    data: [*]Color,
+    alloc: std.mem.Allocator,
+
+    pub fn zeroed(
+        alloc: std.mem.Allocator,
+        dim: [2]u16,
+    ) !@This() {
+        const len = dim[0] * dim[1];
+        const memory = try alloc.alloc(Color, len);
+        for (memory) |*m| {
+            m.* = Color.BLACK;
+        }
+        return @This(){
+            .height = dim[0],
+            .width = dim[1],
+            .data = memory.ptr,
+            .alloc = alloc,
+        };
+    }
+
+    /// Returens number of pixels in image
+    pub fn size(self: @This()) usize {
+        return self.height * self.width;
+    }
+
+    pub fn deinit(self: *@This()) void {
+        self.alloc.free(self.data[0..self.size()]);
+    }
+
+    pub fn get(self: @This(), x: usize, y: usize) Color {
+        std.debug.assert(y < self.height);
+        std.debug.assert(x < self.width);
+        return self.data[self.width * y + x];
+    }
+
+    pub fn get_mut(self: *@This(), x: usize, y: usize) *Color {
+        std.debug.assert(y < self.height);
+        std.debug.assert(x < self.width);
+        return &self.data[self.width * y + x];
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -1,51 +1,22 @@
 const std = @import("std");
+const Image = @import("image.zig").Image;
+const Color = @import("image.zig").Color;
 
-const Image = struct {
-    dim: [2]u16 = .{ 0, 0 },
-    data: [*]u8,
-    alloc: std.mem.Allocator,
-
-    fn zero(
-        alloc: std.mem.Allocator,
-        dim: [2]u16,
-    ) !@This() {
-        const size = dim[0] * dim[1];
-        const memory = try alloc.alloc(u8, size);
-        for (memory) |*m| {
-            m.* = 0;
+pub fn draw_ascii(
+    self: Image,
+    writer: std.io.AnyWriter,
+) void {
+    for (0..self.height) |row| {
+        for (0..self.width) |col| {
+            const symbol: u8 = if (self.get(row, col).as_gray() < @as(usize, 128)) 'X' else '.';
+            writer.print(
+                "{c}",
+                .{symbol},
+            ) catch return;
         }
-        return @This(){
-            .dim = dim,
-            .data = memory.ptr,
-            .alloc = alloc,
-        };
+        writer.print("\n", .{}) catch return;
     }
-
-    fn draw_ascii(
-        self: @This(),
-        writer: std.io.AnyWriter,
-    ) void {
-        for (0..self.dim[0]) |row| {
-            for (0..self.dim[1]) |col| {
-                const symbol: u8 = if (self.data[row * @as(usize, self.dim[1]) + col] < @as(usize, 128)) 'X' else '.';
-                writer.print(
-                    "{c}",
-                    .{symbol},
-                ) catch return;
-            }
-            writer.print("\n", .{}) catch return;
-        }
-    }
-
-    fn deinit(self: *@This()) void {
-        const size = self.dim[0] * self.dim[1];
-        self.alloc.free(self.data[0..size]);
-    }
-
-    fn at(self: *@This(), point: Point(usize)) *u8 {
-        return &self.data[self.dim[1] * point.y + point.x];
-    }
-};
+}
 
 fn Point(comptime inner: type) type {
     return struct {
@@ -55,12 +26,12 @@ fn Point(comptime inner: type) type {
 }
 
 fn draw_circle(image: *Image, center: Point(f32), radius: f32) void {
-    for (0..image.dim[0]) |col| {
-        for (0..image.dim[1]) |row| {
+    for (0..image.height) |col| {
+        for (0..image.width) |row| {
             if (std.math.pow(f32, (center.x - @as(f32, @floatFromInt(col))), 2.0) +
                 std.math.pow(f32, (center.y - @as(f32, @floatFromInt(row))), 2.0) < radius * radius)
             {
-                image.at(.{ .x = col, .y = row }).* = 255;
+                image.get_mut(col, row).* = Color.WHITE;
             }
         }
     }
@@ -68,26 +39,23 @@ fn draw_circle(image: *Image, center: Point(f32), radius: f32) void {
 
 pub fn main() !void {
     // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    var buffer = [_]u8{0} ** 1000;
+    var buffer = [_]u8{0} ** 10000;
     var alloc = std.heap.FixedBufferAllocator.init(buffer[0..]);
-    const a = alloc.allocator();
+
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    var angle: f32 = 0.0;
+    var image = Image.zeroed(alloc.allocator(), .{ 30, 30 }) catch {
+        std.debug.print("Run out of memory 😥\n", .{});
+        return;
+    };
+    defer image.deinit();
 
-    for (0..10000) |_| {
-        var image = try Image.zero(a, .{ 30, 30 });
-        defer image.deinit();
+    draw_circle(&image, .{ .x = 10.0, .y = 10.0 }, 10.0);
+    draw_ascii(image, stdout.any());
+    // var array: [4]u8 = .{ 0, 255, 0, 255 };
+    // (Image{ .size = .{ 2, 2 }, .data = &array }).draw_ascii(stdout.any());
 
-        draw_circle(&image, .{ .x = 10.0 + std.math.sin(angle) * 5, .y = 10.0 + std.math.cos(angle) * 10 }, 10.0);
-        angle += 0.1;
-        image.draw_ascii(stdout.any());
-        // var array: [4]u8 = .{ 0, 255, 0, 255 };
-        // (Image{ .size = .{ 2, 2 }, .data = &array }).draw_ascii(stdout.any());
-
-        try bw.flush(); // don't forget to flush!
-        std.time.sleep(10 * std.time.ns_per_ms);
-    }
+    try bw.flush(); // don't forget to flush!
 }
